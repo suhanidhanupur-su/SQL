8bfc2200c8f15d8cfbd3ef16278ee5e35727611b
Suhani kumari <suhanidhanupur@gmail.com>
2026-01-24 19:06:42 +0530

    commit

--- SQL/emp_table.sql
+++ SQL/emp_table.sql
@@ -1,43 +1,32 @@
-CREATE TABLE student_table (
-    std_id INT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    roll INT NOT NULL,
-    class VARCHAR(50),
-    city VARCHAR(50),
-    reg_no INT UNIQUE
+CREATE TABLE Employee(
+       emp_id INT PRIMARY KEY,
+	   emp_name VARCHAR(50),
+	   emp_dep VARCHAR(50),
+	   salary INT
 );
 
-INSERT INTO student_table (std_id, name, roll, class, city, reg_no) VALUES
-(111, 'Rohit', 2000, '10th', 'Patna', 10001),
-(222, 'Rohan', 2001, '11th', 'Mohali', 10002),
-(333, 'Tridev', 2002, 'BCA', 'Delhi', 10003),
-(444, 'Somu', 2003, 'Plane B.A', 'Sirari', 10004),
-(5555, 'Vicky', 2004, 'B.Tech', 'Bathinda', 10005);
+INSERT INTO Employee VALUES
+(1,'Suhani','IT',45000),
+(2,'Neha','HR',50000),
+(3,'Radha','Finance',55000);
+
+SELECT * FROM Employee;
+
+UPDATE Employee
+SET salary =85000
+WHERE emp_id = 1;
 
 
-select * from student_table;
+DELETE FROM Employee
+WHERE emp_id = 2;
 
-CREATE TABLE library (
-    std_id INT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    issue_book VARCHAR(100)
-);
+ALTER TABLE Employee
+ADD city VARCHAR (50);
 
-INSERT INTO library (std_id, name, issue_book) VALUES
-(111, 'Rohit', 'H.C Verma'),
-(222, 'Rohan', 'Python'),
-(333, 'Tridev', 'R.D Sharma');
+UPDATE Employee
+SET city = 'Noida'
+WHERE emp_id = 1;
 
+TRUNCATE TABLE Employee;
+DROP TABLE  Employee; 
 
-select * from library;
-
-SELECT 
-    student_table.std_id,
-    student_table.name,
-    student_table.class,
-    student_table.city,
-    library.issue_book
-FROM student_table
-left JOIN library
-ON student_table.std_id = library.std_id;
-dim-tvhd-qgo
